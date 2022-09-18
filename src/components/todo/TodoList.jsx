06f43279ccf6119a4d95baff1dd6e7d55e9f6212
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2022-09-18 00:08:06 -0400

    allowed user to map the list

--- src/components/todo/TodoList.jsx
+++ src/components/todo/TodoList.jsx
@@ -9,41 +9,52 @@ import {
 } from "@mantine/core";
 import { IconSquarePlus, IconTrash, IconEdit } from "@tabler/icons";
 
-const TodoList = () => {
+const TodoList = ({ todos, deleteTodo }) => {
   return (
     <>
-      <SimpleGrid
-        cols={2}
-        spacing="lg"
-        breakpoints={[
-          { maxWidth: 980, cols: 3, spacing: "md" },
-          { maxWidth: 755, cols: 2, spacing: "sm" },
-          { maxWidth: 600, cols: 1, spacing: "sm" },
-        ]}
-      >
-        <Box>
-          <Checkbox label="Clone or download repository from GitHub" />
-        </Box>
-        <Box>
-          <Center>
-            <Tooltip label="Edit List Item">
-              <ActionIcon color="blue">
-                <IconEdit />
-              </ActionIcon>
-            </Tooltip>
-            <Tooltip label="Remove Item">
-              <ActionIcon color="red">
-                <IconTrash />
-              </ActionIcon>
-            </Tooltip>
-            <Tooltip label="Add extra items for more complex task">
-              <ActionIcon color="green">
-                <IconSquarePlus />
-              </ActionIcon>
-            </Tooltip>
-          </Center>
-        </Box>
-      </SimpleGrid>
+      {todos.map((todo, index) => {
+        return (
+          <SimpleGrid
+            cols={2}
+            spacing="lg"
+            breakpoints={[
+              { maxWidth: 980, cols: 3, spacing: "md" },
+              { maxWidth: 755, cols: 2, spacing: "sm" },
+              { maxWidth: 600, cols: 1, spacing: "sm" },
+            ]}
+            key={index.toString()}
+          >
+            <Box>
+              <Checkbox label={todo} />
+            </Box>
+            <Box>
+              <Center>
+                <Tooltip label="Edit List Item">
+                  <ActionIcon color="blue">
+                    <IconEdit />
+                  </ActionIcon>
+                </Tooltip>
+                <Tooltip label="Remove Item">
+                  <ActionIcon
+                    color="red"
+                    aria-label="Delete"
+                    onClick={() => {
+                      deleteTodo(index);
+                    }}
+                  >
+                    <IconTrash />
+                  </ActionIcon>
+                </Tooltip>
+                <Tooltip label="Add extra items for more complex task">
+                  <ActionIcon color="green">
+                    <IconSquarePlus />
+                  </ActionIcon>
+                </Tooltip>
+              </Center>
+            </Box>
+          </SimpleGrid>
+        );
+      })}
     </>
   );
 };
